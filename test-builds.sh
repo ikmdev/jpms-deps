12f7010c95e92a4a951cef9c35d8cfb61077c0f2
Justin Grant <jlgrock@gmail.com>
2024-09-30 13:25:04 -0400

    iterating to test workflow bash statements

--- test-builds.sh
+++ test-builds.sh
@@ -8,8 +8,7 @@ function exec_mvn_build() {
   mvn clean install -Dmaven.build.cache.enabled=false --batch-mode -e \
       -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
-
-  size=$(find ./target -maxdepth 1 -type f -name '*[!-javadoc][!-sources].jar' -exec 'jar tf {}\' ';' | grep "dev.ikm.jpms" | wc -l)
+  size=$(find ./target -maxdepth 1 -type f -name '*[!-javadoc][!-sources].jar' -exec 'jar tf {} \' ';' | grep -c 'dev.ikm.jpms')
   if [ "$size" -lt "2" ]; then
     echo "ERROR: jar does not contain ikmdev packages"
     exit 1
